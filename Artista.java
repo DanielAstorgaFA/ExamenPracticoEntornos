13c7c842a3629f62156d90459c1c98cf1f9b2d56
DanielAstorgaFA <daniel.astorga.lopez.alu@iesfernandoaguilar.es>
2024-03-14 16:14:47 +0100

    Añadido metodo esSobrevivire a Artista.java

--- Artista.java
+++ Artista.java
@@ -13,6 +13,14 @@ public class Artista {
                 "nombreArtista='" + nombreArtista + '\'' +
                 '}';
     }
+    /*Método que mira si el nombre de la artista es Mónica Naranjo 
+      y devuelve un booleano en función del resultado: */
+    public boolean esSobrevivire(){
+        boolean esSobrevivire = false;
+        if(nombreArtista.equals("Mónica Naranjo"))
+            esSobrevivire = true;
+        return esSobrevivire;
+    }
 
     public String getNombreArtista() {
         return nombreArtista;
